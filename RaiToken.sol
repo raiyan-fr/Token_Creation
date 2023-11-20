7bd7b5a1663207e999809b1ea05cad26dd19e2a9
Raiyan <132445420+raiyan-fr@users.noreply.github.com>
2023-11-21 04:35:18 +0530

    Update RaiToken.sol

--- RaiToken.sol
+++ RaiToken.sol
@@ -6,8 +6,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RaiTKN is ERC20, Ownable {
     constructor() ERC20("Raik Coin", "RKC") Ownable(msg.sender) {
-        // Mint a hundred tokens with 18 decimals to the contract owner
-        _mint(msg.sender, 100 * 10 ** 18); 
+        // Mint a Thousand tokens with 18 decimals to the contract owner
+        _mint(msg.sender, 1000 * 10 ** 18); 
     }
 
     // Function to mint new tokens
