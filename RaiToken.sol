9cf5c5b178e8109d76c7f09ba5fbc21654224b4b
Raiyan <132445420+raiyan-fr@users.noreply.github.com>
2023-11-22 15:26:04 +0530

    Update RaiToken.sol

--- RaiToken.sol
+++ RaiToken.sol
@@ -8,15 +8,23 @@ contract RaiTKN is ERC20, Ownable {
     constructor() ERC20("Raik Coin", "RKC") Ownable(msg.sender) {
         // Mint a Thousand tokens with 18 decimals to the contract owner
         _mint(msg.sender, 1000 * 10 ** 18); 
-    }
+    }  
 
-    // Function to mint new tokens
-    function MintHundred() public onlyOwner {
-        _mint(msg.sender,100 * 10 ** 18);
-    }
+    // Function to mint new tokens  
+    function MintTokens(uint256 amount) public onlyOwner {
+        _mint(msg.sender, amount);
+    }  
 
-    // function to burn tokens with taking 18 decimals
+    // function to burn tokens 
     function BurnTokens(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "You do not have enough Raik coin to burn");
         _burn(msg.sender, amount);
     }
+    // function to transfer funds
+    function transferTokens(address receiver, uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "You do not have enough Raik coin to transfer");
+        approve(msg.sender, amount);
+        transferFrom(msg.sender, receiver, amount);
+    }
 }
+     
